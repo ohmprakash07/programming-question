31c7d85b4ef1f7db85a871d071c6e8c53820cc6c
ohmprakash07 <ohmprakash7205@gmail.com>
2025-02-16 20:54:36 +0530

    patterns added

---
+++ pattens/1.py
@@ -0,0 +1,6 @@
+#right angle triangle
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=" ")
+    print()
---
+++ pattens/2.py
@@ -0,0 +1,6 @@
+#right angle inverted triangle
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=" ")
+    print()
